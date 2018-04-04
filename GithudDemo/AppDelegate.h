0f392a423a0d1aec11651fd2e2cadb18732dc5ef
王璇 <wangxuan@wangxuandemacbook.local>
2018-04-04 16:39:31 +0800

    Initial Commit

---
+++ GithudDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GithudDemo
+//
+//  Created by 王璇 on 2018/4/4.
+//  Copyright © 2018年 王璇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
